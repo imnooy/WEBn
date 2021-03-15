f3e2c64263e9ddabd64873fdb2829f1ddd68572c
imnooy <imnoooy@gmail.com>
2021-03-15 17:44:52 +0900

    homepage

--- nodejs/Syntax/conditional.js
+++ nodejs/Syntax/conditional.js
@@ -1,5 +1,5 @@
 let args=process.argv; //배열로 입력값 저장
-console.log(args[2]); //node syntax/conditional.js yoonmi k880 라고 입력하면 yoonmi 출력
+//console.log(args[2]); //node syntax/conditional.js yoonmi k880 라고 입력하면 yoonmi 출력
 console.log('A');
 console.log('B');
 if(args[2]==='1') {
--- nodejs/main.js
+++ nodejs/main.js
@@ -6,41 +6,70 @@ let u=require('url'); //모듈 url
 let app = http.createServer(function(request,response){
     let url = request.url;
     let queryData=u.parse(url, true).query;
+    let pathname=u.parse(url, true).pathname;
     let title=queryData.id;
     //console.log(queryData.id); //HTML 출력
-    if(url == '/'){
-        title='Welcome';
-    }
-    if(url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
 
-    //queryString에 따라 읽는 파일의 경로가 달라진다!
-    fs.readFile(`data/${title}`, 'utf8', function(err, description) {
-        let template=`
-        <!doctype html>
-        <html>
-        <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-        </body>
-        </html>
-    `;
-    response.end(template);
-    })
+    console.log(u.parse(url, true).pathname);
+
+    if(pathname==='/') { 
+        if(title===undefined) { //페이지 홈
+            fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+                let t='Welcome';
+                let d='Hello, Node.js';
+                let template=`
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${t}</h2>
+                <p>${d}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            //queryString에 따라 읽는 파일의 경로가 달라진다!
+            fs.readFile(`data/${title}`, 'utf8', function(err, description) {
+                let template=`
+                <!doctype html>
+                <html>
+                <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+                </head>
+                <body>
+                <h1><a href="/">WEB</a></h1>
+                <ul>
+                    <li><a href="/?id=HTML">HTML</a></li>
+                    <li><a href="/?id=CSS">CSS</a></li>
+                    <li><a href="/?id=JavaScript">JavaScript</a></li>
+                </ul>
+                <h2>${title}</h2>
+                <p>${description}</p>
+                </body>
+                </html>
+                `;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    }
+    else {
+        response.writeHead(404);
+        response.end('Not found');
+    }
    
 });
 app.listen(3000);
