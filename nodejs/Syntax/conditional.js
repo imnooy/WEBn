9ad978e4d0c7d5822507f727bc22ecbe23b82725
imnooy <imnoooy@gmail.com>
2021-03-15 17:22:42 +0900

    input in console

---
+++ nodejs/Syntax/conditional.js
@@ -0,0 +1,10 @@
+let args=process.argv; //배열로 입력값 저장
+console.log(args[2]); //node syntax/conditional.js yoonmi k880 라고 입력하면 yoonmi 출력
+console.log('A');
+console.log('B');
+if(args[2]==='1') {
+    console.log('C1');
+} else {
+    console.log('C2');  
+}
+console.log('D');
