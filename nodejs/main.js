a4dfec50288f8a7a46a428400ca0250f20eccdee
imnooy <imnoooy@gmail.com>
2021-03-15 15:58:56 +0900

    App - 동적인 웹페이지 만들기

--- nodejs/main.js
+++ nodejs/main.js
@@ -5,9 +5,10 @@ let u=require('url'); //모듈 url
 let app = http.createServer(function(request,response){
     let url = request.url;
     let queryData=u.parse(url, true).query;
+    let title=queryData.id;
     console.log(queryData.id); //HTML 출력
     if(url == '/'){
-      url = '/index.html';
+        title='Welcome';
     }
     if(url == '/favicon.ico'){
         response.writeHead(404);
@@ -15,10 +16,29 @@ let app = http.createServer(function(request,response){
         return;
     }
     response.writeHead(200);
-    response.end(queryData.id); //queryData.id인 'HTML'만 웹 화면에 표시
-    //localhost:3000/?id=css로 주소를 바꾸면 'css'만 화면에 표시된다.
+    let template=`
+        <!doctype html>
+        <html>
+        <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+            <li><a href="/?id=HTML">HTML</a></li>
+            <li><a href="/?id=CSS">CSS</a></li>
+            <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+        <img src="coding.jpg" width="100%">
+        </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+        </p>
+        </body>
+        </html>
 
-    //response.end(fs.readFileSync(__dirname + url));
-    //프로그래밍적으로 사용자에게 전송할 데이터를 생성할 수 있다!
+    `;
+    response.end(template);
 });
 app.listen(3000);
