45094ac990d6fcd924b9ea80f0cc57a1347bc51e
imnooy <imnoooy@gmail.com>
2021-03-15 15:23:53 +0900

    Syntax
    javascript syntax

---
+++ nodejs/Syntax/template.js
@@ -0,0 +1,15 @@
+let n='k8805';
+let letter='Dear ' + n +'\n\
+\n\hihihihihihihihihih'; 
+
+let letter1=`Dear ${n}
+
+hihihihhihihihihihihihihh`;
+
+//줄 바꿈을 편리하게
+
+console.log(letter);
+
+let a=1;
+let b='1';
+
---
+++ nodejs/main.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request,response){
+    var url = request.url;
+    if(request.url == '/'){
+      url = '/index.html';
+    }
+    if(request.url == '/favicon.ico'){
+        response.writeHead(404);
+        response.end();
+        return;
+    }
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+    //프로그래밍적으로 사용자에게 전송할 데이터를 생성할 수 있다!
+});
+app.listen(3000);
