a018bdaab0cc89ec4ab966387accd7b5de1b0b33
imnooy <imnoooy@gmail.com>
2021-03-20 19:08:38 +0900

    data parse, objectification
    check -> pm2 log

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 let u=require('url'); //모듈 url
+let qs=require('querystring');
 //모듈: 기본적으로 제공하는 기능들을 그룹핑 해놓은 각각의 그룹들
 
 function templateHTML(title, list, body) {
@@ -81,7 +82,7 @@ let app = http.createServer(function(request,response){
             let t='WEB - create';
             let list=templateList(filelist);         
             let template=templateHTML(title, list, `
-            <form action="http://localhost:3000/process_create" method="POST">
+            <form action="http://localhost:3000/create_process" method="POST">
             <p></p><input type="text" name="title" placeholder="title"></p>
             <p>
                 <textarea name="description" id="" cols="30" rows="10" placeholder="description"></textarea>
@@ -92,7 +93,28 @@ let app = http.createServer(function(request,response){
             </form>`);
             response.writeHead(200);
             response.end(template);
-            })    
+            })  
+              
+    }
+    else if(pathname === '/create_process') {
+        let body='';
+        request.on('data', function(data) {
+            body=body+data;
+            //callback이 실행될 때마다 data를 추가
+        });
+        //특정한 양의 data를 서버에서 수신할 때마다 callback func를 호출하도록 약속 되어있음.
+        request.on('end', function() {
+            //더 이상 들어올 data가 없을 때 이 func 수행
+            let post=qs.parse(body);
+            //console.log(post);
+            console.log(post.title);
+            //object!
+
+            let ti=post.title;
+            let de=post.description;
+        });
+        response.writeHead(200);
+        response.end('success');
     }
     else {
         response.writeHead(404);
