39dfdf69ed8863a357c41d05d2e4522701389498
imnooy <imnoooy@gmail.com>
2021-04-02 19:24:02 +0900

    Update main.js

--- nodejs/BOARD/main.js
+++ nodejs/BOARD/main.js
@@ -1,7 +1,11 @@
-const http=require('http');
+const http=require('http'); //http라는 모듈을 요구.
+const template=require('./lib/template.js');
 
 const server=http.createServer( function(request, response) { //웹 서버 객체 생성
-    
+
+    const html=template.html();
+    response.writeHead(200); //응답 200번
+    response.end(html); //표시할 내용
 });
 
 server.listen(3001);
