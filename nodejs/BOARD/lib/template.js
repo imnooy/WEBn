7cb90907ac906777f967806b2e21914da63d9faf
imnooy <imnoooy@gmail.com>
2021-04-03 19:35:51 +0900

    style

---
+++ nodejs/BOARD/lib/template.js
@@ -0,0 +1,16 @@
+module.exports={
+    html:function() {
+        return `
+        <!doctype html>
+        <html>
+        <head>
+        <link type="text/css" href="lib/style.css">
+        <title>yoonmi's board❤️</title>
+        <meta charset="utf-8">
+        </head>
+        <body>
+        <h1 class="title"><a href="/">BOARD</a></h1>
+        </body>
+        </html>`;
+    }
+}
